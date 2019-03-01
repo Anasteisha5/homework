644dca4fa8e1dc55650bd87bf5537adc5ffc4990
Anastasiya Shamilova <nastyaespano22@yandex.ru>
2019-03-01 13:22:34 +0300

    complete task1 & task2

--- variables/main.js
+++ variables/main.js
@@ -5,3 +5,14 @@ let z = 0;
 result = x * y + 5 * z + x - 1;
 
 console.log(result);
+
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+averageRating = (algebra + geography + physics) / 3;
+console.log(averageRating); 
+
+name = "Anastasiya"
+console.log(`Привет, мир! Меня зовут ${name}`); 
